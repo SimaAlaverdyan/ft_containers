b9b0221e7240ed9c5173a046b2cc812982bd1a37
SimaAlaverdyan <sima.alaverdyan.02@gmail.com>
2022-12-29 17:58:00 +0400

    random

--- iterators/random_access_iterator.hpp
+++ iterators/random_access_iterator.hpp
@@ -6,7 +6,13 @@ namespace ft
     template <typename T>
 	class	random_access_iterator : public ft::iterator<ft::random_access_iterator_tag, T>
 	{
-        
+        public:
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::iterator_category iterator_category;
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::value_type value_type;
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::difference_type difference_type;
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::pointer pointer;
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::reference reference;
+            typedef pointer iterator_type;
     };
 }
 
